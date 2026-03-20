eba6b723f1af0f7e300e0f37f0d6f0fc274e62f7
Miguel-Silva-Alves <devmigueldev@gmail.com>
2026-03-20 11:44:23 -0300

    :sparkles: feat: add banner with 3 images from canva

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import "./banner.css";
 
 function Banner() {
-    // JSX
-    return (
-        <header className="banner">
-        <img src="/images/bannerifsp.png" alt="imagem aerea" />
-        </header>
-    );
+  const images = [
+    "/images/banner1.png",
+    "/images/banner2.png",
+    "/images/banner3.png"
+  ];
+
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 3000); // troca a cada 3s
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <header className="banner">
+      <div
+        className="banner-track"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {images.map((img, index) => (
+          <img key={index} src={img} alt={`banner-${index}`} />
+        ))}
+      </div>
+    </header>
+  );
 }
+
 export default Banner;
